66cb0b416782821ef8c59223da6f15cb730aec1d
jboude <boudejerome@gmail.com>
2023-10-25 10:59:03 +0200

    add start btn functionnality

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,13 +7,20 @@ let movementKeys = {
     right: "d"
 }
 
+const gameStart = () => {
+    let mainMenu = document.querySelector(".main-menu");
+    let gameWindow = document.querySelector(".game");
+    mainMenu.classList.toggle("hidden");
+    gameWindow.classList.toggle("hidden");
+}
+
 const displayDifficultyPopup = () => {
     const popup = document.querySelector(".difficulty-popup");
     popup.classList.toggle("hidden");
 }
 
-
 document.addEventListener("DOMContentLoaded", () => {
+
     document.querySelector(".menu-content .difficulty-btn").addEventListener("click", displayDifficultyPopup)
     /* window.addEventListener("keydown", (event) => {
         let key = event.key;
@@ -22,4 +29,6 @@ document.addEventListener("DOMContentLoaded", () => {
         key == movementKeys.left ? console.log("left") :
         key == movementKeys.right && console.log("right")
     }) */
+
+    document.querySelector(".start-btn").addEventListener("click", gameStart);
 })
