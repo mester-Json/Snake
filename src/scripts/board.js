ad7786f3f83dc19ae8e7e10e5bca35505c3de415
jboude <boudejerome@gmail.com>
2023-10-26 10:09:15 +0200

    add function to create board

--- src/scripts/board.js
+++ src/scripts/board.js
@@ -1,26 +1,9 @@
-snakeGame.getBlockWidth = () => {
-    return blockWidth;
+const createBoard = () => {
+    const board = document.querySelector("#board");
+    const board_ctx = board.getContext("2d");
+
+    board_ctx.canvas.height = snakeGame.BOARD_HEIGHT;
+    board_ctx.canvas.width = snakeGame.BOARD_WIDTH;
 }
 
-snakeGame.getBlockHeight = () => {
-    return blockHeight;
-}
-
-const createPlayingField = () => {
-    fieldGrid = [];
-    numfieldCols = fieldWidth / getBlockWidth;
-    numfieldRows = fieldHeight / blockHeight;
-
-    for (let row = 0; row < numfieldRows; row++) {
-        fieldGrid[row] = [];
-        for (let col = 0; col < numfieldCols; col++) {
-            if (col === 0 || row === 0 || col === (numfieldCols - 1) || row === (numfieldRows - 1)) {
-                fieldGrid[row][col] = 1;
-            } else {
-                fieldGrid[row][col] = 0;
-            }
-        }
-    }
-}
-
-export { createPlayingField }
+export { createBoard }
